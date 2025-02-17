ae17b324961e34b5b499440eb042f33c0e07e1f8
ice <ice_hotf@proton.me>
2025-02-17 14:34:26 +0100

    Update uninstall_DZL.sh

--- uninstall_DZL.sh
+++ uninstall_DZL.sh
@@ -5,8 +5,8 @@
 sudo rm /home/$USER/.local/share/applications/DZL.desktop 
 sudo rm /usr/share/icons/dzl.png
 sudo rm -rf /home/$USER/DZL/
-sudo rm -rf /home/$USER/DZL-Beta-Linux-main/
-sudo rm /home/$USER/DZL-Beta-Linux-main.zip
+sudo rm -rf /home/$USER/DZL-Beta-SteamDeck-main/
+sudo rm /home/$USER/DZL-Beta-SteamDeck-main.zip
 cd ~
 exec bash
 echo "Uninstallation Successful"
