8fcc36ab06bb9190ccfaf51c336d76fb5d07957c
ice <ice_hotf@proton.me>
2025-02-20 00:07:03 +0100

    Update install_DZL.sh

--- install_DZL.sh
+++ install_DZL.sh
@@ -1,45 +1,45 @@
 #!/bin/bash
 
+
 # Get & set Current Directory as a Variable:
 pwd=$(pwd list)
+
 
 ### SteamOS Read-Only Disable ###
 sudo steamos-readonly disable
 
+
 ### Give user ownership to DZL Files ###
-
 echo "Taking Ownership of the DZL Folder Contents."
-
 sudo chmod +x /home/$USER/DZL-Beta-SteamDeck-main/DZL/./*
 
+
 ### DZL Installer ### 
-
 echo "Copying 'DZL Folder' to /home/$USER/"
-
 cp -R /$pwd/DZL/ /home/$USER/
 
 echo ""
 echo ""
 
-### Copy DZL.desktop file to your application Directory ###
 
+### Copy DZL.desktop file to your application Directory ###
 cp /home/$USER/DZL/DZL.desktop /home/$USER/.local/share/applications/DZL.desktop &
 echo "DZL.desktop was copied to '/home/$USER/.local/share/applications/' and should be available in the application menu."
 
 echo ""
 echo ""
 
-### Copy dzl.png file to /usr/share/icons ###
 
+### Copy dzl.png file to /usr/share/icons ###
 sudo cp /home/$USER/DZL/dzl.png /usr/share/icons/
 echo "Application Icon copied to 'usr/share/icons'"
 
 echo ""
 echo ""
 
+
 ### SteamOS Read-Only Enable ###
 sudo steamos-readonly enable
-
 echo ""
 echo ""
 echo "Launch DZL by Using the Application Menu Entry or through Terminal: bash /home/$USER/DZL/DZL.sh"
