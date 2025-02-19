d8d9c39f37d4d95854da8f014e7218b496a9fb31
ice <ice_hotf@proton.me>
2025-02-20 00:08:35 +0100

    Update uninstall_DZL.sh

--- uninstall_DZL.sh
+++ uninstall_DZL.sh
@@ -3,16 +3,15 @@
 ### SteamOS Read-Only Disable ###
 sudo steamos-readonly disable
 
+
 ### Uninstall DZL###
-
 sudo rm /home/$USER/.local/share/applications/DZL.desktop 
-sudo rm /usr/share/icons/dzl.png
-sudo rm -rf /home/$USER/DZL/
-sudo rm -rf /home/$USER/DZL-Beta-SteamDeck-main/
-sudo rm /home/$USER/DZL-Beta-SteamDeck-main.zip
-cd ~
-exec bash
-### SteamOS Read-Only Enable ###
-sudo steamos-readonly enable
+echo "Removing DZL.Desktop from '.local/share/applications'"
 
-echo "Uninstallation Successful!"
+sudo rm /usr/share/icons/dzl.png
+echo "Removing dzl.png from 'usr/share/icons'"
+
+sudo rm -rf /home/$USER/DZL/
+echo "Removing folder 'DZL' from 'home/$USER/DZL'"
+
+echo "Uninstallation Successful"
